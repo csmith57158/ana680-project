3a9bb7741d33cab78a495bca8dd71a7fe53fdf79
test <smity3sd@gmail.com>
2024-03-07 11:25:04 -0800

    Initialize Git Repository

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pickle
 import numpy as np
 
 app = Flask(__name__)
-model = pickle.load(open('svm_model.pkl', 'rb'))
+model = pickle.load(open('svm_model.pkl', 'rb'))  
 
 @app.route('/')
 def home():
@@ -12,15 +12,14 @@ def home():
 @app.route('/predict', methods=['POST'])
 def predict():
     # Extract feature values from the form
-    feature_values = [request.form.get(feature) for feature in ['fixed_acidity', 'volatile_acidity', 'citric_acid', 'residual_sugar',
-                                                                'chlorides', 'free_sulfur_dioxide', 'total_sulfur_dioxide', 'density',
-                                                                'pH', 'sulphates', 'alcohol']]
-    # Convert feature values to float and create a numpy array for prediction
+    feature_values = [request.form.get(feature) for feature in ['Sex', 'Length', 'Diameter', 'Height',
+                                                                'Whole_weight', 'Shucked_weight', 'Viscera_weight', 'Shell_weight']]
+    # Convert feature values to float, reshape
     features_array = np.array([float(value) for value in feature_values if value]).reshape(1, -1)
-    # Predict the quality using your model
+    # Predict the Rings using your model
     prediction = model.predict(features_array)
     # Render the result
-    return render_template('index.html', prediction_text=f'Predicted Quality: {prediction[0]}')
+    return render_template('index.html', prediction_text=f'Predicted Rings: {prediction[0]}')
 
 if __name__ == "__main__":
     app.run(debug=True)
